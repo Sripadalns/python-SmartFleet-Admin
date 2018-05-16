82122253aa4691f10ba2db24e06f86010b61addf
Sripadalns <sripadalns@gmail.com>
2018-05-16 10:16:04 +0530

    Tracar API function

---
+++ Device_Add.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 03 14:02:23 2018
+
+@author: sripals
+"""
+import sys
+import urllib
+import urllib2
+import json
+
+debug = '-v' in sys.argv
+baseUrl = "http://track.intracworks.com"
+user = { 'email' : 'admin', 'password' : 'jaihanuman4US$' }
+
+def login():
+    request = urllib2.Request(baseUrl + '/api/session')
+    response = urllib2.urlopen(request, urllib.urlencode(user))
+    if debug:
+        print ('\nlogin: %s\n' % repr(json.load(response)))
+    return response.headers.get('Set-Cookie')
+
+def add_device(cookie, name,unique_id):
+    request = urllib2.Request(baseUrl + '/api/devices')
+    request.add_header('Cookie', cookie)
+    request.add_header('Content-Type', 'application/json')
+    device = { 'name' : name, 'uniqueId' : unique_id,'category' : 'Bus' }
+    response = urllib2.urlopen(request, json.dumps(device))
+    data = json.load(response)
+    return data['id']
+
+#cookie = login()
+#add_device(cookie,382506)
