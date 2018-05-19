40a3e06e933c126dc407998c4955430517fb3d82
Sripadalns <sripadalns@gmail.com>
2018-05-19 19:26:49 +0530

    Added Logfile based view

--- putty.py
+++ putty.py
@@ -1,5 +1,6 @@
 import ssh
 import paramiko
+import datetime
 ssh = paramiko.SSHClient()
 hostname = "159.89.170.250"
 username="root"
@@ -9,7 +10,20 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 x= ssh.connect(hostname,22,username,password)
 ssh.invoke_shell()
 stdin, stdout, stderr = ssh.exec_command (command)
+x= stdout.read()
 print stdout.read()
+print x
 
+text_file = open("Output.txt", "a")
+datex = str(datetime.date.today())
+text_file.write(datex)
+text_file.write("\n")
+timex=str(datetime.datetime.now().time())
+text_file.write(timex)
+text_file.write("\n")
+text_file.write(x)
+text_file.write("***********************\n")
+
+text_file.close()
 print x
 
